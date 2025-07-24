96fa38a930895d0e9383b44ef7ad0bdbdaf90ef3
xiaohanihan <hudson.wyatt@outlook.com>
2025-07-25 01:23:26 +0800

    first page animation

--- packages/web/src/app/invite/layout.tsx
+++ packages/web/src/app/invite/layout.tsx
@@ -1,27 +1,25 @@
-import AppTheme from '@/shared-theme/AppTheme';
-import '../globals.css';
-import './invite.css';
-import { NextAuthProvider } from '@/components/NextAuthProvider';
-import { Nunito } from 'next/font/google';
+import AppTheme from '@/shared-theme/AppTheme'
+import '../globals.css'
+import './invite.css'
+import { NextAuthProvider } from '@/components/NextAuthProvider'
+import { Nunito } from 'next/font/google'
 
 const nunito = Nunito({
   subsets: ['latin'],
   weight: ['400', '500', '600', '700', '800'],
   variable: '--font-nunito', // CSS 变量名
-});
+})
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <html lang="en" className={nunito.variable}>
-      <body
-        className={`${nunito.className} !bg-green01-500 relative overflow-hidden min-h-screen w-screen`}
-      >
+      <body className={`${nunito.className} relative overflow-hidden min-h-screen w-screen`}>
         <NextAuthProvider>
           <AppTheme>{children}</AppTheme>
         </NextAuthProvider>
       </body>
     </html>
-  );
-};
+  )
+}
 
-export default Layout;
+export default Layout
