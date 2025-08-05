088b75b728a782fd5910b0b4adef93f8f10f8611
xiaohanihan <hudson.wyatt@outlook.com>
2025-08-05 16:08:06 +0800

    insert breakline inside twitter text

--- packages/web/src/lib/constants.ts
+++ packages/web/src/lib/constants.ts
@@ -1,5 +1,8 @@
 export const REFERRAL_CODE_SEARCH_PARAM = 'referralCode'
 export const copyText = `Juice early. Juice loud. Juice for real.
+
 @commidotfun connects memes, communities, and rewards — in real-time.
-I’m in the Commi beta — whitelist claimed, rewards loading... 🍹:
+
+I'm in the Commi beta — whitelist claimed, rewards loading... 🍹:
+
 Start juicing 👉: `
